eb9dc8de9176acddec3bc22080b0b63561defee1
virtuoso <rogervirtuoso@gmail.com>
2019-11-19 11:56:53 +1300

    WIP

--- code/LocationMapPage.php
+++ code/LocationMapPage.php
@@ -26,8 +26,8 @@ class LocationMapPage_Controller extends Page_Controller {
 
     public function locationData() {
         // Get the locations from the database, exclude any that don't have LatLng's defined
-		$infoWindowList = Location::get()->exclude(array('lat' => null, 'lng' => null));
-//        $infoWindowList = Location::get();
+//		$infoWindowList = Location::get()->exclude(array('lat' => null, 'lng' => null));
+        $infoWindowList = Location::get();
         if ($infoWindowList) {
             $InfoWindows = array();
             foreach ($infoWindowList as $obj) {
