f5fff1d6d8b42ddfcc1b339b3a770a40deb4e4dc
virtuoso <rogervirtuoso@gmail.com>
2019-11-27 14:07:31 +1300

    Hotfix

--- javascript/GoogleMapConfig.js
+++ javascript/GoogleMapConfig.js
@@ -28,7 +28,7 @@ function initialize() {
     // the ajax object, populated with address and infoWindow
     $.ajax({
         // url: window.location.href + 'locationData',
-        url: 'http://localhost:9981/new-location-map-page-2/locationData',
+        url: window.location.href +'/new-location-map-page-2/locationData',
         type: 'GET',
         success: function (result) {
             var locations = JSON.parse(result);
