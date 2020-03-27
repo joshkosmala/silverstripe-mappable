796ae10406c236101aa2c393906c9be3c1366a0e
virtuoso <rogervirtuoso@gmail.com>
2020-03-27 17:03:19 +1300

    Phone book mappable

--- code/LocationMapPage.php
+++ code/LocationMapPage.php
@@ -20,52 +20,52 @@ class LocationMapPage_Controller extends Page_Controller {
     }
 
     private static $allowed_actions = array(
-        'locationData', 'importAddressFile', 'getLocationFromAddress', 'populateLocation'
+        'locationData', 'getLocationFromAddress', 'populateLocation'
     );
 
     //To the import process run effectively, the file needs to be exported as the database colummn sequence ordered. eg: Name, Email, Phone, Address, City
     //IMPORTANT: Always check the database on connection credentials line
-    public function importAddressFile()
-    {
-        $conn = mysqli_connect("localhost", "root", "root", "northtel2");
-
-        if (isset($_POST["file"])) {
-
-            $fileName = $_POST["file"];
-
-            $file = fopen($fileName, "r");
-
-            while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
-                if ($column[0] == 'Type' || $column[0] == 'First Name' || $column[0] == 'Name') {
-                    continue;
-                }
-
-                $name = $column[0];
-                if (!empty($column[0])) {
-                    $name = strstr($column[0], "'") ? str_replace("'", "''", $column[0]) : $column[0];
-                    $name = preg_replace('/[^0-9\'/\']/g', '', $name);
-                }
-                $northtelClients = NorthtelClients::get()->where(" Name = '" . $name . "' OR Email = '" . $column[1] . "'")->exists();
-                if (!$northtelClients) {
-                    $sqlInsert = "INSERT into NorthtelClients (Name, Email, PhoneNumber, Address, City)
-                   values ('" . $name . "','" . $column[1] . "','" . $column[2] . "','" . $column[5] . "','" . $column[6] . "')";
-                    $result = mysqli_query($conn, $sqlInsert);
-
-                    if (!empty($result)) {
-                        $type = "success";
-                        $message = "CSV Data Imported into the Database";
-                    } else {
-                        $type = "error";
-                        $message = "Problem in Importing CSV Data";
-                    }
-                } else {
-                    continue;
-                }
-            }
-            fclose($file);
-            $this->populateLocation();
-        }
-    }
+//    public function importAddressFile()
+//    {
+//        $conn = mysqli_connect("localhost", "root", "root", "northtel2");
+//
+//        if (isset($_POST["file"])) {
+//
+//            $fileName = $_POST["file"];
+//
+//            $file = fopen($fileName, "r");
+//
+//            while (($column = fgetcsv($file, 10000, ",")) !== FALSE) {
+//                if ($column[0] == 'Type' || $column[0] == 'First Name' || $column[0] == 'Name') {
+//                    continue;
+//                }
+//
+//                $name = $column[0];
+//                if (!empty($column[0])) {
+//                    $name = strstr($column[0], "'") ? str_replace("'", "''", $column[0]) : $column[0];
+//                    $name = preg_replace('/[^0-9\'/\']/g', '', $name);
+//                }
+//                $northtelClients = NorthtelClients::get()->where(" Name = '" . $name . "' OR Email = '" . $column[1] . "'")->exists();
+//                if (!$northtelClients) {
+//                    $sqlInsert = "INSERT into NorthtelClients (Name, Email, PhoneNumber, Address, City)
+//                   values ('" . $name . "','" . $column[1] . "','" . $column[2] . "','" . $column[5] . "','" . $column[6] . "')";
+//                    $result = mysqli_query($conn, $sqlInsert);
+//
+//                    if (!empty($result)) {
+//                        $type = "success";
+//                        $message = "CSV Data Imported into the Database";
+//                    } else {
+//                        $type = "error";
+//                        $message = "Problem in Importing CSV Data";
+//                    }
+//                } else {
+//                    continue;
+//                }
+//            }
+//            fclose($file);
+//            $this->populateLocation();
+//        }
+//    }
 
     public function populateLocation()
     {
@@ -127,13 +127,6 @@ class LocationMapPage_Controller extends Page_Controller {
             $geometry = $element['geometry'];
             $location = $geometry['location'];
         }
-
-        //If $contents is not a boolean FALSE value.
-//        if($contents !== false){
-//            //Print out the contents.
-//        }
-
-//        return json_encode($location);
         return $location;
     }
 
