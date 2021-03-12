68a8f5b7365c524d1e7a67df3114d4ed15e80628
virtuoso <rogervirtuoso@gmail.com>
2021-03-12 13:27:33 +1300

    Layout mobile fix

--- javascript/GoogleMapConfig.js
+++ javascript/GoogleMapConfig.js
@@ -80,13 +80,17 @@ function detectBrowser() {
     var useragent = navigator.userAgent;
     var mapdiv = document.getElementById("map_canvas");
 
-    if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
-        mapdiv.style.width = '100%';
-        mapdiv.style.height = '100%';
-    } else {
-        mapdiv.style.width = '100%';
-        mapdiv.style.height = '500px';
-    }
+    mapdiv.style.width = '100%';
+    mapdiv.style.height = '500px';
+
+    //TODO: Check below
+    // if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
+    //     mapdiv.style.width = '100%';
+    //     mapdiv.style.height = '100%';
+    // } else {
+    //     mapdiv.style.width = '100%';
+    //     mapdiv.style.height = '500px';
+    // }
 }
 
 function addMarkerWithTimeout(position, map, infowindow, location, timeout) {
